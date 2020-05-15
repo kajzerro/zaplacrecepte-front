6b5f0a325e07c803a67e98b76d883ecbfcdfdf28
Mateusz Kaszyk <mateuszkaszyk@gmail.com>
2020-05-15 23:14:44 +0200

    Polish texts in login page and logo

--- src/component/loginPage/LoginPage.js
+++ src/component/loginPage/LoginPage.js
@@ -4,15 +4,17 @@ import './LoginPage.css';
 function LoginPage() {
     return (
         <div className="wrapper">
-            <div id="formContent">
-                <form>
-                    <input type="text" id="login" className="second" name="login" placeholder="login"></input>
+
+        <div id="formContent">
+        <div className="icon"> <img src="/logo.png" alt="Smiley face" height="80" width="80"/> </div>
+        <form>
+                    <input type="text" id="login" className="second" name="login" placeholder="Nazwa gabinetu"></input>
                     <input type="text" id="password" className="third" name="login"
-                           placeholder="password"></input>
-                    <a href='./lista'> <input type="button" className="fourth" value="Log In"></input></a>
+                           placeholder="Hasło"></input>
+                    <a href='./lista'> <input type="button" className="fourth" value="Zaloguj się"></input></a>
                 </form>
                 <div id="formFooter">
-                    <a className="underlineHover" href="#">Forgot Password?</a>
+                    <a className="underlineHover" href="#">Zapomniałaś/zapomniałeś hasła?</a>
                 </div>
             </div>
         </div>
