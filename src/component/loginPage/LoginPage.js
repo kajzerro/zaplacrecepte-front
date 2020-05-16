058324ce5631ac55af29ceb725a94d766249cc6b
Jakub Osika <osika.jakub@gmail.com>
2020-05-16 23:14:47 +0200

    Fix changing URL paths

--- src/component/loginPage/LoginPage.js
+++ src/component/loginPage/LoginPage.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import {useHistory} from "react-router-dom";
 import './LoginPage.css';
 
 function LoginPage() {
+    let history = useHistory();
     return (
         <div className="login-page">
             <div className="wrapper">
@@ -9,10 +11,11 @@ function LoginPage() {
                     <div className="icon"><img src="/logo.png" alt="Smiley face" height="80" width="80"/></div>
                     <form>
                         <input type="text" id="login" className="second" name="login"
-                               placeholder="Nazwa gabinetu"></input>
+                               placeholder="Nazwa gabinetu"/>
                         <input type="text" id="password" className="third" name="login"
-                               placeholder="Hasło"></input>
-                        <a href='./lista'> <input type="button" className="fourth" value="Zaloguj się"></input></a>
+                               placeholder="Hasło"/>
+                        <input type="button" className="fourth" value="Zaloguj się"
+                               onClick={() => history.push('/lista')}/>
                     </form>
                     <div id="formFooter">
                         <a className="underlineHover" href="#">Zapomniałaś/zapomniałeś hasła?</a>
