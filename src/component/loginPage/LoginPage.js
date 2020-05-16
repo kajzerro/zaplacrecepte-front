1effe6138b135e051964d8c0b354d964af7440d7
Jakub Osika <osika.jakub@gmail.com>
2020-05-16 21:52:01 +0200

    Login page css fixes

--- src/component/loginPage/LoginPage.js
+++ src/component/loginPage/LoginPage.js
@@ -3,18 +3,20 @@ import './LoginPage.css';
 
 function LoginPage() {
     return (
-        <div className="wrapper">
-
-        <div id="formContent">
-        <div className="icon"> <img src="/logo.png" alt="Smiley face" height="80" width="80"/> </div>
-        <form>
-                    <input type="text" id="login" className="second" name="login" placeholder="Nazwa gabinetu"></input>
-                    <input type="text" id="password" className="third" name="login"
-                           placeholder="Hasło"></input>
-                    <a href='./lista'> <input type="button" className="fourth" value="Zaloguj się"></input></a>
-                </form>
-                <div id="formFooter">
-                    <a className="underlineHover" href="#">Zapomniałaś/zapomniałeś hasła?</a>
+        <div className="login-page">
+            <div className="wrapper">
+                <div id="formContent">
+                    <div className="icon"><img src="/logo.png" alt="Smiley face" height="80" width="80"/></div>
+                    <form>
+                        <input type="text" id="login" className="second" name="login"
+                               placeholder="Nazwa gabinetu"></input>
+                        <input type="text" id="password" className="third" name="login"
+                               placeholder="Hasło"></input>
+                        <a href='./lista'> <input type="button" className="fourth" value="Zaloguj się"></input></a>
+                    </form>
+                    <div id="formFooter">
+                        <a className="underlineHover" href="#">Zapomniałaś/zapomniałeś hasła?</a>
+                    </div>
                 </div>
             </div>
         </div>
