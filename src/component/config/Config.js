512e5b1b03decb1ac1aa03b6a609e0302434e4f1
Jakub Osika <osika.jakub@gmail.com>
2020-06-05 20:23:30 +0200

    Select backend env dynamically

--- src/component/config/Config.js
+++ src/component/config/Config.js
@@ -2,5 +2,5 @@ export function getEndpoint() {
     if(process.env.REACT_APP_ENV === "local") {
         return "http://localhost:5000";
     }
-    return "https://api.zaplacrecepte.pl";
+    return "https://api." + (new URL(window.location.href).hostname);
 }
