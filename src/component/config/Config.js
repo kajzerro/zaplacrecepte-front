ea99507e291f6ec4d2be23408b00f30cc386e11d
Jakub Osika <osika.jakub@gmail.com>
2020-06-28 09:32:21 +0200

    Buggy backend selection fix

--- src/component/config/Config.js
+++ src/component/config/Config.js
@@ -2,5 +2,5 @@ export function getEndpoint() {
     if(process.env.REACT_APP_ENV === "local") {
         return "http://localhost:5000";
     }
-    return "https://api." + (new URL(window.location.href).hostname);
+    return "https://api." + (new URL(window.location.href).hostname).replace("www.", "");
 }
