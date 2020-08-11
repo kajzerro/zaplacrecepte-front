8ef3a0ee153552505517b5c897a11d6a986d7215
Jakub Osika <osika.jakub@gmail.com>
2020-08-11 18:22:11 +0200

    Show cancelled page to the client

--- src/component/paymentPage/PaymentPage.js
+++ src/component/paymentPage/PaymentPage.js
@@ -5,62 +5,71 @@ import {useParams} from 'react-router-dom'
 import axios from "axios";
 import {getEndpoint} from "../config/Config";
 
-function PaymentPage(props) {
+function PaymentPage() {
 
-  const [accepted, setAccepted] = useState(false);
-  const [clientData, setClientData] = useState({});
-  let { prescriptionId } = useParams();
+    const [accepted, setAccepted] = useState(false);
+    const [clientData, setClientData] = useState({});
+    let {prescriptionId} = useParams();
 
-  const handlePay = () => {
-  };
+    const handlePay = () => {
+    };
 
-  function fetchData() {
-    axios.get(getEndpoint() + "/api/prescriptions/client/" + prescriptionId)
-    .then(res => {
-      setClientData(res.data);
-    })
-    .catch(err => {
-      alert(err);
-    });
-  }
+    function fetchData() {
+        axios.get(getEndpoint() + "/api/prescriptions/client/" + prescriptionId)
+            .then(res => {
+                setClientData(res.data);
+            })
+            .catch(err => {
+                alert(err);
+            });
+    }
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+    useEffect(() => {
+        fetchData();
+    }, []);
 
-  return (
-      <div className="container payment-page">
-        <div className="row mb-lg-5 mt-3 mb-sm-2">
-          <h4 className="text-center">Szczegóły Twojego zamówienia sporządzonego przez
-            gabinet Dr Marka Krzystyniaka</h4>
-        </div>
-        <div className="row">
-          <div className="offset-lg-3 col-lg-6 offset-sm-12 col-sm-12">
-            <PrescriptionFields disabled={true} key={clientData.id} onChange={() => {
-            }} initData={clientData} checkAll={false}/>
-            <Form.Check
-                className="mb-4 mt-4"
-                type="checkbox"
-                id="myid"
-            >
-              <Form.Check.Input type="checkbox"
-                                onChange={e => {
-                                  console.log(e.target.checked);
-                                  setAccepted(e.target.checked);
-                                }} />
-              <Form.Check.Label>Oświadczam, że zapoznałem/am się z <a href="/regulamin">regulaminem</a> i akceptuje go</Form.Check.Label>
-            </Form.Check>
-            <a href={ clientData.orderUrl}>
-            <button className="mb-3 btn btn-success btn-block patient-button" disabled={!accepted} onClick={handlePay}>
-              Akceptuj zamówienie i przejdź do płatności
-            </button>
-            </a>
-          </div>
-        </div>
-        <div className="row">
-        </div>
-      </div>
-  );
+    return (
+        (clientData && clientData.status === 'CANCELED') ?
+            <div className="payment-page-cancel">
+                <h4>
+                    Płatność za receptę została anulowana, prosimy o kontakt z recepcją.
+                </h4>
+            </div>
+            :
+            <div className="container payment-page">
+                <div className="row mb-lg-5 mt-3 mb-sm-2">
+                    <h4 className="text-center">Szczegóły Twojego zamówienia sporządzonego przez
+                        gabinet Dr Marka Krzystyniaka</h4>
+                </div>
+                <div className="row">
+                    <div className="offset-lg-3 col-lg-6 offset-sm-12 col-sm-12">
+                        <PrescriptionFields disabled={true} key={clientData.id} onChange={() => {
+                        }} initData={clientData} checkAll={false}/>
+                        <Form.Check
+                            className="mb-4 mt-4"
+                            type="checkbox"
+                            id="myid"
+                        >
+                            <Form.Check.Input type="checkbox"
+                                              onChange={e => {
+                                                  console.log(e.target.checked);
+                                                  setAccepted(e.target.checked);
+                                              }}/>
+                            <Form.Check.Label>Oświadczam, że zapoznałem/am się z <a href="/regulamin">regulaminem</a> i
+                                akceptuje go</Form.Check.Label>
+                        </Form.Check>
+                        <a href={clientData.orderUrl}>
+                            <button className="mb-3 btn btn-success btn-block patient-button" disabled={!accepted}
+                                    onClick={handlePay}>
+                                Akceptuj zamówienie i przejdź do płatności
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div className="row">
+                </div>
+            </div>
+    );
 }
 
 export default PaymentPage;
