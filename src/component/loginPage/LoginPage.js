25a640e787dee287369db0960585b13d3f15cbd9
Jakub Osika <osika.jakub@gmail.com>
2020-09-27 11:24:03 +0200

    Login page scale for Mati screen

--- src/component/loginPage/LoginPage.js
+++ src/component/loginPage/LoginPage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useHistory} from "react-router-dom";
 import './LoginPage.css';
 import axios from 'axios';
@@ -20,66 +20,86 @@ function LoginPage() {
             .catch(err => alert(err));
     }
 
+    useEffect(() => {
+        if (document.documentElement.scrollHeight > window.screen.height) {
+            setScalingStyle({
+                transform: "scale(" + window.screen.height / document.documentElement.scrollHeight + ")",
+                transformOrigin: "top center"
+            });
+        }
+
+    }, []);
+
+    const [scalingStyle, setScalingStyle] = useState({});
+
     const [formLogin, setFormLogin] = useState("");
     const [formPassword, setFormPassword] = useState("");
 
     let history = useHistory();
     return (
-        <div className="login-page container-fluid">
-            <div className="row">
-                <div className="col-6 left-side">
-                    <div className="row login-header">
-                        <div className="col-4">
-                            <img src="loginPage/logo.png"/>
-                        </div>
-                        <div className="col-8">
-                            <ul>
-                                <li onClick={() => history.push('/wspolpraca')}>Współpraca</li>
-                                <li onClick={() => history.push('/kontakt')}>Kontakt</li>
-                                <li onClick={() => history.push('/regulamin')}>Regulamin</li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div className="row body-wrapper">
-                        <div className="offset-1 col-11 offset-xl-3 col-xl-9">
-                            <div className="hello-header">Witaj w ZaplacRecepte.pl</div>
-                            <div className="input-frame">
-                                <div className="hello-description">Aby w pełni móc korzystać z systemu, zaloguj się
-                                    używając
-                                    swojego
-                                    emaila oraz hasła
-                                </div>
-                                <form>
-                                    <label htmlFor="login">Login/Email</label>
-                                    <input type="text" id="login" name="login"
-                                           placeholder="Nazwa gabinetu" onChange={e => setFormLogin(e.target.value)}/>
-                                    <label htmlFor="password">Hasło</label>
-                                    <input type="password" id="password" name="login"
-                                           placeholder="Hasło" onChange={e => setFormPassword(e.target.value)}/>
-                                    <div className="after-input">
-                                        <span className="error-label">*Podałeś nieprawidłowy email lub hasło</span>
-                                        <span className="forgot-label">Zapomniałeś hasła?</span>
-                                        <div className="remember-me">
-                                            <img src="loginPage/checkboxOn.png"/>
-                                            Zapamiętaj mnie
-                                        </div>
-                                    </div>
-                                    <input type="button" value="Zaloguj się"
-                                           onClick={() => login()}/>
-                                </form>
+        <div className="login-page"
+             style={scalingStyle}>
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-6 col-xs-12">
+                        <div className="row login-header">
+                            <div className="col-4">
+                                <img src="loginPage/logo.png"/>
+                            </div>
+                            <div className="col-8">
+                                <ul>
+                                    <li onClick={() => history.push('/wspolpraca')}>Współpraca</li>
+                                    <li onClick={() => history.push('/kontakt')}>Kontakt</li>
+                                    <li onClick={() => history.push('/regulamin')}>Regulamin</li>
+                                </ul>
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="col-6 right-side">
-                    <img src="loginPage/loginImage.png"/>
+                <div className="row">
+                    <div className="col-6">
+                        <div className="row body-wrapper">
+                            <div className="offset-1 col-11 offset-xl-3 col-xl-9">
+                                <div className="hello-header">Witaj w ZaplacRecepte.pl</div>
+                                <div className="input-frame">
+                                    <div className="hello-description">Aby w pełni móc korzystać z systemu, zaloguj się
+                                        używając
+                                        swojego
+                                        emaila oraz hasła
+                                    </div>
+                                    <form>
+                                        <label htmlFor="login">Login/Email</label>
+                                        <input type="text" id="login" name="login"
+                                               placeholder="Nazwa gabinetu"
+                                               onChange={e => setFormLogin(e.target.value)}/>
+                                        <label htmlFor="password">Hasło</label>
+                                        <input type="password" id="password" name="login"
+                                               placeholder="Hasło" onChange={e => setFormPassword(e.target.value)}/>
+                                        <div className="after-input">
+                                            <span className="error-label">*Podałeś nieprawidłowy email lub hasło</span>
+                                            <span className="forgot-label">Zapomniałeś hasła?</span>
+                                            <div className="remember-me">
+                                                <img src="loginPage/checkboxOn.png"/>
+                                                Zapamiętaj mnie
+                                            </div>
+                                        </div>
+                                        <input type="button" value="Zaloguj się"
+                                               onClick={() => login()}/>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6 right-side">
+                        <img src="loginPage/loginImage.png"/>
+                    </div>
                 </div>
-            </div>
-            <div className="right-side-bottom">
-                <div className="inside"/>
             </div>
             <div className="login-footer">
                 All rights reserved © copyright www.zaplacrecepte.pl 2020
+            </div>
+            <div className="right-side-bottom">
+                <div className="inside"/>
             </div>
         </div>
     );
